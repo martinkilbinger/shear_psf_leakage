e2c7c0abafa197863727931f8da4975703ff254f
Sacha Guerrini <sacha.guerrini@cea.fr>
2023-11-15 17:03:04 +0100

    Add modules to plot rho and tau stats for different catalogs allowing smooth comparison. Wrote a script to test the new functionalities.

--- shear_psf_leakage/rho_tau_stat.py
+++ shear_psf_leakage/rho_tau_stat.py
@@ -11,6 +11,157 @@ from astropy.table import Table
 
 import treecorr
 
+def neg_dash(
+    ax,
+    x_in,
+    y_in,
+    yerr_in,
+    vertical_lines=True,
+    xlabel='',
+    ylabel='',
+    rho_nb='',
+    tau_nb='',
+    cat_id='',
+    ylim=None,
+    semilogx=False,
+    semilogy=False,
+    **kwargs
+):
+    r"""Neg Dash.
+
+    This function is for making plots with vertical errorbars,
+    where negative values are shown in absolute value as dashed lines.
+    The resulting plot can either be saved by specifying a file name as
+    ``plot_name``, or be kept as a pyplot instance (for instance to combine
+    several neg dashes).
+
+    Parameters
+    ----------
+    ax : 
+        The matplotlib object on which the plot is performed
+    x_in : numpy.ndarray
+        X-axis inputs
+    y_in : numpy.ndarray
+        Y-axis inputs
+    yerr_in : numpy.ndarray
+        Y-axis error inputs
+    vertical_lines : bool, optional
+        Option to plot vertical lines; default is ``True``
+    xlabel : str, optional
+        X-axis label
+    ylabel : str, optional
+        Y-axis label
+    rho_nb : str, optional
+        Rho number
+    ylim : float, optional
+        Y-axis limit
+    semilogx : bool
+        Option to plot the x-axis in log scale; default is ``False``
+    semilogy : bool
+        Option to plot the y-axis in log scale; default is ``False``
+
+    """
+    x = np.copy(x_in)
+    y = np.copy(y_in)
+    if yerr_in is not None:
+        yerr = np.copy(yerr_in)
+    else:
+        yerr = np.zeros_like(x)
+    # catch and separate errorbar-specific keywords from Lines2D ones
+    safekwargs = dict(kwargs)
+    errbkwargs = dict()
+    if 'linestyle' in kwargs.keys():
+        print(
+            'Warning: linestyle was provided but that would kind of defeat'
+            + 'the purpose, so I will just ignore it. Sorry.'
+        )
+        del safekwargs['linestyle']
+    for errorbar_kword in [
+        'fmt', 'ecolor', 'elinewidth', 'capsize', 'barsabove', 'errorevery'
+    ]:
+        if errorbar_kword in kwargs.keys():
+            # posfmt = '-'+kwargs['fmt']
+            # negfmt = '--'+kwargs['fmt']
+            errbkwargs[errorbar_kword] = kwargs[errorbar_kword]
+            del safekwargs[errorbar_kword]
+    errbkwargs = dict(errbkwargs, **safekwargs)
+
+    # plot up to next change of sign
+    current_sign = np.sign(y[0])
+    first_change = np.argmax(current_sign * y < 0)
+    while first_change:
+        if current_sign > 0:
+            ax.errorbar(
+                x[:first_change],
+                y[:first_change],
+                yerr=yerr[:first_change],
+                linestyle='-',
+                **errbkwargs,
+            )
+            if vertical_lines:
+                ax.vlines(
+                    x[first_change - 1],
+                    0,
+                    y[first_change - 1],
+                    linestyle='-',
+                    **safekwargs,
+                )
+                ax.vlines(
+                    x[first_change],
+                    0,
+                    np.abs(y[first_change]),
+                    linestyle='--',
+                    **safekwargs,
+                )
+        else:
+            ax.errorbar(
+                x[:first_change],
+                np.abs(y[:first_change]),
+                yerr=yerr[:first_change],
+                linestyle='--',
+                **errbkwargs,
+            )
+            if vertical_lines:
+                ax.vlines(
+                    x[first_change - 1],
+                    0,
+                    np.abs(y[first_change - 1]),
+                    linestyle='--',
+                    **safekwargs,
+                )
+                ax.vlines(
+                    x[first_change],
+                    0,
+                    y[first_change],
+                    linestyle='-',
+                    **safekwargs,
+                )
+        x = x[first_change:]
+        y = y[first_change:]
+        yerr = yerr[first_change:]
+        current_sign *= -1
+        first_change = np.argmax(current_sign * y < 0)
+    # one last time when `first_change'==0 ie no more changes:
+    if rho_nb:
+        lab = fr'$\rho_{rho_nb}(\theta)$ '+cat_id
+    elif tau_nb:
+        lab = fr'$\tau_{tau_nb}(\theta)$' +cat_id
+    else:
+        lab = cat_id
+    if current_sign > 0:
+        ax.errorbar(x, y, yerr=yerr, linestyle='-', label=lab, **errbkwargs)
+    else:
+        ax.errorbar(x, np.abs(y), yerr=yerr, linestyle='--', label=lab,
+                     **errbkwargs)
+    if semilogx:
+        ax.set_xscale('log')
+    if semilogy:
+        ax.set_yscale('log')
+    if ylim is not None:
+        ax.set_ylim(ylim)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
 class Catalogs():
     """
     Catalogs
@@ -66,7 +217,7 @@ class Catalogs():
 
         self._output = "."
 
-    def set_params(self, params, output):
+    def set_params(self, params=None, output=None):
         """
         set_params:
 
@@ -168,7 +319,7 @@ class Catalogs():
 
         return ra, dec, g1, g2, weights
     
-    def build_catalog(self, cat_type, key, npatch=None, square_size=False):
+    def build_catalog(self, cat_type, key, npatch=None, square_size=False, mask=False):
         """
         build_catalogue
 
@@ -188,6 +339,9 @@ class Catalogs():
 
         square_size : bool
             If True, the size computed in the catalogue is squared (Default: False)
+
+        mask : bool
+            If True, use PSF and star flags to mask the data. (Default: False)
         """
 
         if npatch is None:
@@ -195,11 +349,20 @@ class Catalogs():
         
         ra, dec, g1, g2, weights = self.get_cat_fields(cat_type, square_size)
 
+        if mask:
+            flag_psf = self.dat_psf[self._params["PSF_flag"]]
+            flag_star = self.dat_psf[self._params["star_flag"]]
+            mask_arr = (flag_psf==0) & (flag_star==0)
+            if weights is not None:
+                weights = weights[mask_arr]
+        else:
+            mask_arr = np.array([True for i in ra])
+
         cat = treecorr.Catalog(
-            ra=ra,
-            dec=dec,
-            g1=g1,
-            g2=g2,
+            ra=ra[mask_arr],
+            dec=dec[mask_arr],
+            g1=g1[mask_arr],
+            g2=g2[mask_arr],
             w=weights,
             ra_units=self._params["ra_units"],
             dec_units=self._params["dec_units"],
@@ -291,7 +454,7 @@ class RhoStat():
         pass
         #TO DO
 
-    def build_cat_to_compute_rho(self, path_cat_star, catalog_id='', square_size=False):
+    def build_cat_to_compute_rho(self, path_cat_star, catalog_id='', square_size=False, mask=False):
         """
         build_cat_to_compute_rho
 
@@ -305,6 +468,9 @@ class RhoStat():
 
         square_size : bool
             If True, the size computed in the catalogue is squared (Default: False)
+
+        mask : bool
+            If True, use PSF and star flags to mask the data. (Default: False)
         """
 
         self.catalogs.read_shear_cat(path_gal=None, path_psf=path_cat_star)
@@ -312,9 +478,9 @@ class RhoStat():
         if self.verbose:
             print("Building catalogs...")
 
-        self.catalogs.build_catalog(cat_type='psf', key='psf_'+catalog_id, square_size=square_size)
-        self.catalogs.build_catalog(cat_type='psf_error', key='psf_error_'+catalog_id, square_size=square_size)
-        self.catalogs.build_catalog(cat_type='psf_size_error', key='psf_size_error_'+catalog_id, square_size=square_size)
+        self.catalogs.build_catalog(cat_type='psf', key='psf_'+catalog_id, square_size=square_size, mask=mask)
+        self.catalogs.build_catalog(cat_type='psf_error', key='psf_error_'+catalog_id, square_size=square_size, mask=mask)
+        self.catalogs.build_catalog(cat_type='psf_size_error', key='psf_size_error_'+catalog_id, square_size=square_size, mask=mask)
 
         if self.verbose:
             print("Catalogs successfully built...")
@@ -335,7 +501,7 @@ class RhoStat():
             The path where the rho stats will be saved.
         """
         if self.verbose:
-            print("Computation of the rho statistics in progress...")
+            print("Computation of the rho statistics of "+catalog_id+" in progress...")
         rho_0 = treecorr.GGCorrelation(self._treecorr_config)
         rho_0.process(self.catalogs.get_cat('psf_'+catalog_id), self.catalogs.get_cat('psf_'+catalog_id))
         rho_1 = treecorr.GGCorrelation(self._treecorr_config)
@@ -413,19 +579,65 @@ class RhoStat():
         self.load_rho_stats(filename)
 
     def save_rho_stats(self, filename):
-        self.rho_stats.write(filename, format='fits', overwrite=True)
+        self.rho_stats.write(self.catalogs._output+'/'+filename, format='fits', overwrite=True)
 
     def load_rho_stats(self, filename):
-        self.rho_stats = fits.getdata(filename)
+        self.rho_stats = fits.getdata(self.catalogs._output+'/'+filename)
 
-    def plot_rho_stats(self):
+    def plot_rho_stats(self, filenames, colors, catalog_ids, abs=True, savefig=None):
         """
         plot_rho_stats
 
-        Method to plot Rho + statistics.
+        Method to plot Rho + statistics of several catalogues given in argument. Figures are saved in PNG format.
+
+        Parameters:
+        ----------
+        filenames : list str
+            List of the files containing the rho statistics. They can be computed using the method `compute_rho_stats` of this class.
+        
+        colors : list str
+            Color of the plot for the different catalogs. We recommend using different colors for different catalogs for readability.
+
+        catalogs_id : list str
+            A list of catalogs id to label accurately the legend.
+
+        abs : bool
+            If True, plot the absolute value of the rho-statistics. Otherwise, plot the negative values with dashed lines.
+
+        savefig : str
+            If not None, saves the figure with the name given in savefig.
         """
 
-        pass
+        fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(15,9))
+        ax = ax.flatten()
+
+        for filename, color, cat_id in zip(filenames, colors, catalog_ids): #Plot for the different catalogs
+            self.load_rho_stats(filename)
+
+            for i in range(6):
+                xlabel=r"$\theta$ [arcmin]" if i>2 else ''
+                ylabel=r"$\rho-$statistics" if (i==0 or i==3) else ''
+                label = fr'$\rho_{i}(\theta)$ '+cat_id
+                if abs:
+                    ax[i].errorbar(self.rho_stats['theta'], np.abs(self.rho_stats['rho_'+str(i)+'_p']), yerr=np.sqrt(self.rho_stats['varrho_'+str(i)+'_p']),
+                    label=label, color=color, capsize=2)
+                    ax[i].set_xlabel(xlabel)
+                    ax[i].set_ylabel(ylabel)
+                    ax[i].set_xscale('log')
+                    ax[i].set_yscale('log')
+                else:
+                    #Plot the negative values of the rho-stats in dashed lines
+                    neg_dash(
+                        ax[i], self.rho_stats['theta'], self.rho_stats['rho_'+str(i)+'_p'], yerr_in=np.sqrt(self.rho_stats['varrho_'+str(i)+'_p']),
+                        vertical_lines=False, rho_nb=str(i), cat_id=cat_id, xlabel=xlabel, ylabel=ylabel, semilogx=True, semilogy=True, capsize=True, color=color
+                    )
+
+                ax[i].set_xlim(self._treecorr_config["min_sep"], self._treecorr_config["max_sep"])
+                ax[i].legend(loc='upper right')
+
+        if savefig is not None:
+            plt.savefig(self.catalogs._output+'/'+savefig)
+
 
 
 class TauStat():
@@ -448,7 +660,7 @@ class TauStat():
                 "dec_units": "deg",
                 "sep_units": "arcmin",
                 "min_sep": 0.1,
-                "max_sep": 100,
+                "max_sep": 200,
                 "nbins": 20,
                 "var_method": "jackknife"
             }
@@ -457,7 +669,7 @@ class TauStat():
 
         self.verbose = verbose
 
-    def build_cat_to_compute_tau(self, path_cat, cat_type, catalog_id='', square_size=False):
+    def build_cat_to_compute_tau(self, path_cat, cat_type, catalog_id='', square_size=False, mask=False):
         """
         build_cat_to_compute_tau
 
@@ -474,6 +686,9 @@ class TauStat():
 
         square_size : bool
             If True, the size computed in the catalogue is squared (Default: False)
+
+        mask : bool
+            If True, use PSF and star flags to mask the data. (Default: False)
         """
 
         if cat_type=="psf":
@@ -482,9 +697,9 @@ class TauStat():
             if self.verbose:
                 print("Building catalogs...")
 
-            self.catalogs.build_catalog(cat_type='psf', key='psf_'+catalog_id, square_size=square_size)
-            self.catalogs.build_catalog(cat_type='psf_error', key='psf_error_'+catalog_id, square_size=square_size)
-            self.catalogs.build_catalog(cat_type='psf_size_error', key='psf_size_error_'+catalog_id, square_size=square_size)
+            self.catalogs.build_catalog(cat_type='psf', key='psf_'+catalog_id, square_size=square_size, mask=mask)
+            self.catalogs.build_catalog(cat_type='psf_error', key='psf_error_'+catalog_id, square_size=square_size, mask=mask)
+            self.catalogs.build_catalog(cat_type='psf_size_error', key='psf_size_error_'+catalog_id, square_size=square_size, mask=mask)
 
         else:
             self.catalogs.read_shear_cat(path_gal=path_cat, path_psf=None)
@@ -513,7 +728,7 @@ class TauStat():
             The path where the rho stats will be saved.
         """
         if self.verbose:
-            print("Computation of the tau statistics in progress...")
+            print("Computation of the tau statistics of "+catalog_id+" in progress...")
         tau_0 = treecorr.GGCorrelation(self._treecorr_config)
         tau_0.process(self.catalogs.get_cat('gal_'+catalog_id), self.catalogs.get_cat('psf_'+catalog_id))
         tau_2 = treecorr.GGCorrelation(self._treecorr_config)
@@ -561,10 +776,64 @@ class TauStat():
         self.load_tau_stats(filename)
 
     def save_tau_stats(self, filename):
-        self.tau_stats.write(filename, format='fits', overwrite=True)
+        self.tau_stats.write(self.catalogs._output+'/'+filename, format='fits', overwrite=True)
 
     def load_tau_stats(self, filename):
-        self.tau_stats = fits.getdata(filename)
+        self.tau_stats = fits.getdata(self.catalogs._output+'/'+filename)
 
-    def plot_tau_stats(self):
-        pass
+    def plot_tau_stats(self, filenames, colors, catalog_ids, savefig=None, plot_tau_m=True):
+        """
+        plot_tau_stats
+
+        Method to plot Tau + (and -) statistics of several catalogues given in argument. Figures are saved in PNG format.
+
+        Parameters:
+        ----------
+        filenames : list str
+            List of the files containing the rho statistics. They can be computed using the method `compute_rho_stats` of this class.
+        
+        colors : list str
+            Color of the plot for the different catalogs. We recommend using different colors for different catalogs for readability.
+
+        catalogs_id : list str
+            A list of catalogs id to label accurately the legend.
+
+        savefig : str
+            If not None, saves the figure with the name given in savefig.
+
+        plot_tau_m : bool
+            If True, plot the tau - additionally.
+        """
+
+        nrows=1 + plot_tau_m
+
+        fig, ax = plt.subplots(nrows=nrows, ncols=3, figsize=(15,9))
+
+        if nrows==1:
+            ax = ax.reshape(1, 3)
+
+        for filename, color, cat_id in zip(filenames, colors, catalog_ids): #Plot for the different catalogs
+            self.load_tau_stats(filename)
+
+            for i in range(3):
+                for j in range(nrows):
+                    xlabel=r"$\theta$ [arcmin]" if (j==nrows-1) else ''
+                    ylabel=r"$\tau-$statistics" if (i==0) else ''
+                    p_or_m = 'm' if j else 'p'
+                    p_or_m_label = '-' if j else '+'
+                    factor_theta = np.ones_like(self.tau_stats["theta"]) if i==0 else self.tau_stats["theta"]
+                    y = self.tau_stats['tau_'+str(int(0.5*i**2+1.5*i))+'_'+p_or_m]*factor_theta
+                    yerr_in = np.sqrt(self.tau_stats['vartau_'+str(int(0.5*i**2+1.5*i))+'_'+p_or_m])*factor_theta
+                    label = rf'$\tau_{{{int(0.5*i**2+1.5*i)}, {p_or_m_label}}}(\theta)$ '+cat_id if i==0 else rf'$\tau_{{{int(0.5*i**2+1.5*i)}, {p_or_m_label}}}(\theta)\theta$ '+cat_id
+
+                    ax[j, i].errorbar(self.tau_stats["theta"], y, yerr=yerr_in, label=label, color=color, capsize=2)
+                    ax[j, i].set_xlim(self._treecorr_config["min_sep"], self._treecorr_config["max_sep"])
+                    ax[j, i].set_xlabel(xlabel)
+                    ax[j, i].set_ylabel(ylabel)
+                    ax[j, i].set_xscale('log')
+                    ax[j, i].legend(loc='upper right')
+
+        if savefig is not None:
+            plt.savefig(self.catalogs._output+'/'+savefig)
+
+            
--- tests/test_rho_tau_stats.py
+++ tests/test_rho_tau_stats.py
@@ -6,20 +6,88 @@ import matplotlib.pyplot as plt
 from rho_tau_stat import RhoStat, TauStat
 
 
-#Run tests on shapepipe V1
+#Run tests on shapepipe V1 and DESY3
 
-path_gal = "/n17data/mkilbing/astro/data/CFIS/v1.0/ShapePipe/unions_shapepipe_extended_2022_v1.1.fits"
-path_psf = "/n17data/mkilbing/astro/data/CFIS/v1.0/ShapePipe/unions_shapepipe_psf_2022_v1.0.2.fits"
+#First specify  the paths to your data
+paths_gal = [
+    "/n17data/mkilbing/astro/data/CFIS/v1.0/ShapePipe/unions_shapepipe_extended_2022_v1.1.fits",
+    "/home/mkilbing/astro/data/DES/DES_Y3_cut.fits",
+    "/home/hervas/n23_fhervas/shapepipe_clean_uppercut/unions_shapepipe_extended_2022_v1.0.4_theli_4096.fits"
+]
+paths_psf = [
+    "/n17data/mkilbing/astro/data/CFIS/v1.0/ShapePipe/unions_shapepipe_psf_2022_v1.0.2.fits",
+    "/home/mkilbing/astro/data/DES/psf_y3a1-v29.fits",
+    "/n17data/mkilbing/astro/data/CFIS/v1.0/ShapePipe/unions_shapepipe_psf_2022_v1.0.2.fits"
+]
 
-rho_stat_handler = RhoStat(verbose=True)
+#Specify if you want to mask some data using flags
+masks = [
+    True,
+    False,
+    True
+]
 
-rho_stat_handler.build_cat_to_compute_rho(path_psf, catalog_id='SPV1', square_size=True)
+#Specify is the sizes have to be squared. It can depend on the catalog.
+square_sizes = [
+    True,
+    False,
+    True
+]
 
-rho_stat_handler.compute_rho_stats('SPV1', 'rho_stats_spv1.fits')
+#Contains the params of your catalog. Don't forget to specify them since they can vary between different catalogs
+params_des = {
+    "e1_col": "e1_cal",
+    "e2_col": "e2_cal",
+    "w_col": "w",
+    "ra_col": "RA",
+    "dec_col": "Dec",
+    "e1_PSF_col": "piff_e1",
+    "e2_PSF_col": "piff_e2",
+    "e1_star_col": "obs_e1",
+    "e2_star_col": "obs_e2",
+    "PSF_size": "piff_T",
+    "star_size": "obs_T",
+    "output_dir": ".",
+    "patch_number": 120,
+    "ra_units": "deg",
+    "dec_units": "deg" 
+}
+
+params = [
+    None,
+    params_des,
+    None
+]
+
+colors = ['blue', 'red', 'green'] #Colors for the plot
+catalog_ids = ['SPV1', 'DESY3', 'SPV104'] #Ids of the catalogs
+
+rho_stat_handler = RhoStat(verbose=True) #Create your class to compute, save, load and plot rho_stats
+
+for path_gal, path_psf, cat_id, param, mask, square_size in zip(paths_gal, paths_psf, catalog_ids, params, masks, square_sizes): #Iterate on the different catalogs
+
+    if param is None:
+        rho_stat_handler.catalogs.params_default()
+    else:
+        rho_stat_handler.catalogs.set_params(param) #Set the right parameters
+    rho_stat_handler.build_cat_to_compute_rho(path_psf, catalog_id=cat_id, square_size=square_size, mask=mask) #Build the different catalogs
+    rho_stat_handler.compute_rho_stats(cat_id, 'rho_stats_'+cat_id+'.fits') #Compute and save the rho statistics
+
+filenames = ['rho_stats_'+cat_id+'.fits' for cat_id in catalog_ids]
+
+rho_stat_handler.plot_rho_stats(filenames, colors, catalog_ids, abs=True, savefig='rho_stats.png') #Plot
+
+tau_stat_handler = TauStat(catalogs=rho_stat_handler.catalogs, verbose=True) #Create your class to compute, save, load and plot tau_stats
+
+for path_gal, path_psf, cat_id, param, mask, square_size in zip(paths_gal, paths_psf, catalog_ids, params, masks, square_sizes): #Iterate on the catalogs
+
+    if param is None:
+        tau_stat_handler.catalogs.params_default()
+    else:
+        tau_stat_handler.catalogs.set_params(param) #Set the parameters
+    tau_stat_handler.build_cat_to_compute_tau(path_gal, cat_type='gal', catalog_id=cat_id, square_size=square_size, mask=mask) #Build the catalog of galaxies. PSF was computed above
+    tau_stat_handler.compute_tau_stats(cat_id, 'tau_stats_'+cat_id+'.fits') #Compute and save the tau statistics
 
 
-tau_stat_handler = TauStat(catalogs=rho_stat_handler.catalogs, verbose=True)
-
-tau_stat_handler.build_cat_to_compute_tau(path_gal, cat_type='gal', catalog_id='SPV1', square_size=True)
-
-tau_stat_handler.compute_tau_stats('SPV1', 'tau_stats_spv1.fits')
+filenames = ['tau_stats_'+cat_id+'.fits' for cat_id in catalog_ids]
+tau_stat_handler.plot_tau_stats(filenames, colors, catalog_ids, savefig='tau_stats.png', plot_tau_m=False) #Plot
